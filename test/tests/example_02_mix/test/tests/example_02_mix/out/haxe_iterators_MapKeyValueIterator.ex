9c1842d15dd1b4df00213de6facbb36c56ce5ac0
Marcelo Serpa <boss@fullofcaffeine.com>
2025-08-10 18:10:00 -0600

    Checkpoint

---
+++ test/tests/example_02_mix/test/tests/example_02_mix/out/haxe_iterators_MapKeyValueIterator.ex
@@ -0,0 +1,10 @@
+defmodule MapKeyValueIterator do
+  @moduledoc """
+  MapKeyValueIterator module generated from Haxe
+  
+  
+	This Key/Value iterator can be used to iterate across maps.
+
+  """
+
+end
