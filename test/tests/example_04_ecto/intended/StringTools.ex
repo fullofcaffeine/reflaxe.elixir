999f6ddc1eaad5de5688010178bec6d4119c3357
Marcelo Serpa <boss@fullofcaffeine.com>
2025-08-10 18:17:35 -0600

    Checkpoint

---
+++ test/tests/example_04_ecto/intended/StringTools.ex
@@ -0,0 +1,34 @@
+defmodule StringTools do
+  @moduledoc """
+  StringTools module generated from Haxe
+  
+  
+	This class provides advanced methods on Strings. It is ideally used with
+	`using StringTools` and then acts as an [extension](https://haxe.org/manual/lf-static-extension.html)
+	to the `String` class.
+
+	If the first argument to any of the methods is null, the result is
+	unspecified.
+
+  """
+
+  # Static functions
+  @doc "
+		Concatenates `c` to `s` until `s.length` is at least `l`.
+
+		If `c` is the empty String `""` or if `l` does not exceed `s.length`,
+		`s` is returned unchanged.
+
+		If `c.length` is 1, the resulting String length is exactly `l`.
+
+		Otherwise the length may exceed `l`.
+
+		If `c` is null, the result is unspecified.
+	"
+  @spec lpad(TInst(String,[]).t(), TInst(String,[]).t(), TAbstract(Int,[]).t()) :: TInst(String,[]).t()
+  def lpad(arg0, arg1, arg2) do
+    # TODO: Implement function body
+    nil
+  end
+
+end
---
+++ test/tests/router/intended/haxe_iterators_ArrayIterator.ex
@@ -0,0 +1,10 @@
+defmodule ArrayIterator do
+  @moduledoc """
+  ArrayIterator module generated from Haxe
+  
+  
+	This iterator is used only when `Array<T>` is passed to `Iterable<T>`
+
+  """
+
+end
