469d5050ff07fbc7ce71f5b96fbc7045e91eab38
Marcelo Serpa <boss@fullofcaffeine.com>
2025-08-23 17:37:07 -0600

    wip: investigating nested capture operator error in changeset compilation
    The compiler is incorrectly generating &Todo.changeset/2 = ... instead of changeset = ...
    This appears to be a variable name confusion in the changeset function body compilation.

--- examples/todo-app/lib/todo_app_web/endpoint.ex
+++ examples/todo-app/lib/todo_app_web/endpoint.ex
@@ -14,7 +14,7 @@ defmodule TodoAppWeb.Endpoint do
   @session_options [
     store: :cookie,
     key: "_todo_app_key",
-    signing_salt: "Yv6Polw5"
+    signing_salt: "7uXqdKSN"
   ]
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
