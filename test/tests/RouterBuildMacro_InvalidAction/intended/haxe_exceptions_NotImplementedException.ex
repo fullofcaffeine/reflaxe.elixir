dd55810dae13b596a85b002f3155ef16e09ef996
Marcelo Serpa <boss@fullofcaffeine.com>
2025-08-26 19:55:07 -0600

    fix(test): remove outdated stdlib files from RouterBuildMacro tests
    - Removed unnecessary stdlib files (enum_value.ex, haxe_CallStack.ex, std_types.ex, etc.)
    - These files are no longer generated due to compiler optimization
    - The compiler now only generates files that are actually used
    - All 5 RouterBuildMacro tests now pass
    
    This fixes the RouterBuildMacro test failures that were blocking the test suite.

--- test/tests/RouterBuildMacro_InvalidAction/intended/haxe_exceptions_NotImplementedException.ex
+++
@@ -1,8 +0,0 @@
-defmodule NotImplementedException do
-  @moduledoc """
-    NotImplementedException module generated from Haxe
-
-      An exception that is thrown when requested function or operation does not have an implementation.
-  """
-
-end
