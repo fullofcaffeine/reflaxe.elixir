49d4a3ac3f0d62d34a181f84fef4886f3411ff10
Marcelo Serpa <boss@fullofcaffeine.com>
2025-08-28 13:02:45 -0600

    cleanup: remove ad-hoc test files from todo-app
    - Removed compilation_output.txt
    - Removed test_enum.exs
    - Removed test_topic.exs
    - Removed test_native.hx
    
    These were temporary test files created during development

--- examples/todo-app/test_enum.exs
+++
@@ -1,9 +0,0 @@
-# Test the topic_to_string function
-result = TodoPubSub.topic_to_string(:todo_updates)
-IO.puts("Result for :todo_updates = #{inspect(result)}")
-
-result2 = TodoPubSub.topic_to_string(:user_activity)  
-IO.puts("Result for :user_activity = #{inspect(result2)}")
-
-result3 = TodoPubSub.topic_to_string(:system_notifications)
-IO.puts("Result for :system_notifications = #{inspect(result3)}")
