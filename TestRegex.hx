c8e7cff195d2ab02817684e8fd50c8353b4bfe63
Marcelo Serpa <boss@fullofcaffeine.com>
2025-08-28 12:33:05 -0600

    cleanup: remove ad-hoc test files from root directory
    - Removed 18 Test*.hx and test*.hx files
    - Removed 12 Test*.hxml and test*.hxml configuration files
    - Removed obsolete MacroTimeTest, RouterCompilerTest, SimpleExampleTest configs
    - These were temporary test files created during development

--- TestRegex.hx
+++
@@ -1,8 +0,0 @@
-class TestRegex {
-    static function main() {
-        var line = '\t"id": 23,';
-        var regex = ~/^\s*"id"\s*:\s*\d+/;
-        trace("Line: " + line);
-        trace("Matches: " + regex.match(line));
-    }
-}
