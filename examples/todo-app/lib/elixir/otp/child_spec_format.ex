2215c957facf605b6dbe35a26278c11e718e6715
Marcelo Serpa <boss@fullofcaffeine.com>
2025-08-31 13:06:37 -0600

    fix: add EDefmodule case to AST transformer for proper recursion
    - Fixed critical bug where transformAST wasn't recursing into module bodies
    - Added handling for FStatic field access in extractEnumTag to properly detect enum constructors from static methods
    - OTP child specs now transform correctly from tuples to bare module names
    - TypeSafeChildSpec.pubSub() and similar methods now generate proper idiomatic output

--- examples/todo-app/lib/elixir/otp/child_spec_format.ex
+++ examples/todo-app/lib/elixir/otp/child_spec_format.ex
@@ -1,6 +1,6 @@
 defmodule ChildSpecFormat do
   def module_ref(arg0) do
-    {:ModuleRef, arg0}
+    arg0
   end
   def module_with_args(arg0, arg1) do
     {:ModuleWithArgs, arg0, arg1}
@@ -9,6 +9,6 @@ defmodule ChildSpecFormat do
     {:ModuleWithConfig, arg0, arg1}
   end
   def full_spec(arg0) do
-    {:FullSpec, arg0}
+    arg0
   end
 end
