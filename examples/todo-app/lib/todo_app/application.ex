dff34fb4209a20e404eb2c9fc01cb35eb7e5e186
Marcelo Serpa <boss@fullofcaffeine.com>
2025-08-31 18:22:48 -0600

    refactor(todo-app): simplify app name handling
    Removed getAppName() static method to avoid module resolution issues.
    Using direct string literal for app name instead of function call.

--- examples/todo-app/lib/todo_app/application.ex
+++ examples/todo-app/lib/todo_app/application.ex
@@ -1,9 +1,6 @@
 defmodule TodoApp.Application do
-  defp get_app_name() do
-    "TodoApp"
-  end
   def start(type, args) do
-    app_name = Application.get_app_name()
+    app_name = "TodoApp"
     children = [TodoApp.PubSub, TodoAppWeb.Telemetry, TodoAppWeb.Endpoint]
     opts = %{:strategy => :OneForOne, :max_restarts => 3, :max_seconds => 5}
     supervisor_result = Supervisor.start_link(children, opts)
