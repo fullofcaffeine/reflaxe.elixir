973be147fd8d8019cbdf89841d242b9d969939fc
Marcelo Serpa <boss@fullofcaffeine.com>
2025-09-01 03:36:00 -0600

    feat: add User schema and database migrations for Ecto integration

--- examples/todo-app/lib/todo_app/repo.ex
+++ examples/todo-app/lib/todo_app/repo.ex
@@ -1,47 +1,5 @@
 defmodule TodoApp.Repo do
-  def get(queryable, id) do
-    throw(NotImplementedException.new("Repo.get - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 36, :className => "server.infrastructure.Repo", :methodName => "get"}))
-  end
-  def get_by(queryable, conditions) do
-    throw(NotImplementedException.new("Repo.get_by - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 43, :className => "server.infrastructure.Repo", :methodName => "get_by"}))
-  end
-  def all(query) do
-    throw(NotImplementedException.new("Repo.all - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 50, :className => "server.infrastructure.Repo", :methodName => "all"}))
-  end
-  def one(query) do
-    throw(NotImplementedException.new("Repo.one - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 57, :className => "server.infrastructure.Repo", :methodName => "one"}))
-  end
-  def one_not_null(query) do
-    throw(NotImplementedException.new("Repo.one_not_null - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 64, :className => "server.infrastructure.Repo", :methodName => "one_not_null"}))
-  end
-  def exists(query) do
-    throw(NotImplementedException.new("Repo.exists - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 71, :className => "server.infrastructure.Repo", :methodName => "exists"}))
-  end
-  def insert(changeset) do
-    throw(NotImplementedException.new("Repo.insert - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 78, :className => "server.infrastructure.Repo", :methodName => "insert"}))
-  end
-  def insert_not_null(changeset) do
-    throw(NotImplementedException.new("Repo.insert_not_null - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 85, :className => "server.infrastructure.Repo", :methodName => "insert_not_null"}))
-  end
-  def update(changeset) do
-    throw(NotImplementedException.new("Repo.update - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 92, :className => "server.infrastructure.Repo", :methodName => "update"}))
-  end
-  def update_not_null(changeset) do
-    throw(NotImplementedException.new("Repo.update_not_null - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 99, :className => "server.infrastructure.Repo", :methodName => "update_not_null"}))
-  end
-  def delete(record) do
-    throw(NotImplementedException.new("Repo.delete - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 106, :className => "server.infrastructure.Repo", :methodName => "delete"}))
-  end
-  def delete_not_null(record) do
-    throw(NotImplementedException.new("Repo.delete_not_null - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 113, :className => "server.infrastructure.Repo", :methodName => "delete_not_null"}))
-  end
-  def preload(record, associations) do
-    throw(NotImplementedException.new("Repo.preload - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 120, :className => "server.infrastructure.Repo", :methodName => "preload"}))
-  end
-  def transaction(fun) do
-    throw(NotImplementedException.new("Repo.transaction - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 127, :className => "server.infrastructure.Repo", :methodName => "transaction"}))
-  end
-  def rollback(value) do
-    throw(NotImplementedException.new("Repo.rollback - implemented by Ecto.Repo", nil, %{:fileName => "src_haxe/server/infrastructure/Repo.hx", :lineNumber => 134, :className => "server.infrastructure.Repo", :methodName => "rollback"}))
-  end
+  use Ecto.Repo,
+    otp_app: :todo_app,
+    adapter: Ecto.Adapters.Postgres
 end
