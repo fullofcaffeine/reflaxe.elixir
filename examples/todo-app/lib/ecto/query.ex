b416e7f9bae8ce602e11cb1bb9bed26d145d88c2
Marcelo Serpa <boss@fullofcaffeine.com>
2025-09-10 09:51:49 -0600

    fix: parameter prefixing issue causing undefined variables
    - Remove underscore prefixing from ElixirASTBuilder TFunction
    - Let PrefixUnusedParameters transformer be sole authority on underscores
    - Add EStructUpdate detection to properly mark struct params as used
    - Fix TThis to use actual tracked receiver parameter name
    - Fixes AlterTableBuilder compilation errors with undefined variables
    
    The issue was duplicate responsibility - both builder and transformer
    were trying to prefix parameters, causing head/body mismatches.

--- examples/todo-app/lib/ecto/query.ex
+++ examples/todo-app/lib/ecto/query.ex
@@ -1,11 +1,3 @@
 defmodule Query do
-  def from(schema) do
-    query = Ecto.Queryable.to_query(schema)
-    this1 = query
-    this1
-  end
-  def where_all(_query, _conditions) do
-    this1 = elixir_query
-    this1
-  end
+  nil
 end
