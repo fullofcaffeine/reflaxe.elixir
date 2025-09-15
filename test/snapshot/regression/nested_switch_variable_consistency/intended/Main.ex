8712657525f3eb86ce196afa80932912a57162d1
Marcelo Serpa <boss@fullofcaffeine.com>
2025-09-15 12:03:34 -0600

    refactor: continue Phase 0A - more idiomatic pattern updates
    - Fix string concatenation to use interpolation in regression tests
    - Clean up nested case statements with direct pattern matching
    - Remove elem() extraction in favor of destructuring
    - Simplify case statement formatting for readability

--- test/snapshot/regression/nested_switch_variable_consistency/intended/Main.ex
+++ test/snapshot/regression/nested_switch_variable_consistency/intended/Main.ex
@@ -1,42 +1,27 @@
 defmodule Main do
   defp parse_message(msg) do
-    case (msg) do
-      "alert" ->
-        {:some, {:system_alert, "Warning"}}
-      "create" ->
-        {:some, {:todo_created, %{:id => 1, :title => "Test"}}}
-      "delete" ->
-        {:some, {:todo_deleted, 1}}
-      "update" ->
-        {:some, {:todo_updated, %{:id => 1, :title => "Updated"}}}
-      _ ->
-        {:none}
+    case msg do
+      "alert" -> {:some, {:system_alert, "Warning"}}
+      "create" -> {:some, {:todo_created, %{:id => 1, :title => "Test"}}}
+      "delete" -> {:some, {:todo_deleted, 1}}
+      "update" -> {:some, {:todo_updated, %{:id => 1, :title => "Updated"}}}
+      _ -> {:none}
     end
   end
   def main() do
     msg = "create"
-    g = parse_message(msg)
-    case (g) do
-      {:some, value} ->
-        g = g
-        parsed_msg = g
-        case (parsed_msg) do
+    parsed_result = parse_message(msg)
+    case parsed_result do
+      {:some, parsed_msg} ->
+        case parsed_msg do
           {:todo_created, todo} ->
-            g = elem(parsed_msg, 1)
-            todo = g
-            Log.trace("Todo created: " <> Std.string(todo.title), %{:file_name => "Main.hx", :line_number => 51, :class_name => "Main", :method_name => "main"})
+            Log.trace("Todo created: #{todo.title}", %{:file_name => "Main.hx", :line_number => 51, :class_name => "Main", :method_name => "main"})
           {:todo_updated, todo} ->
-            g = elem(parsed_msg, 1)
-            todo = g
-            Log.trace("Todo updated: " <> Std.string(todo.title), %{:file_name => "Main.hx", :line_number => 53, :class_name => "Main", :method_name => "main"})
+            Log.trace("Todo updated: #{todo.title}", %{:file_name => "Main.hx", :line_number => 53, :class_name => "Main", :method_name => "main"})
           {:todo_deleted, id} ->
-            g = elem(parsed_msg, 1)
-            id = g
-            Log.trace("Todo deleted: " <> Kernel.to_string(id), %{:file_name => "Main.hx", :line_number => 55, :class_name => "Main", :method_name => "main"})
+            Log.trace("Todo deleted: #{id}", %{:file_name => "Main.hx", :line_number => 55, :class_name => "Main", :method_name => "main"})
           {:system_alert, message} ->
-            g = elem(parsed_msg, 1)
-            message = g
-            Log.trace("Alert: " <> message, %{:file_name => "Main.hx", :line_number => 57, :class_name => "Main", :method_name => "main"})
+            Log.trace("Alert: #{message}", %{:file_name => "Main.hx", :line_number => 57, :class_name => "Main", :method_name => "main"})
         end
       {:none} ->
         Log.trace("No message parsed", %{:file_name => "Main.hx", :line_number => 60, :class_name => "Main", :method_name => "main"})
