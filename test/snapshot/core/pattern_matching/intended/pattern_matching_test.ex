4c6b41675c5cfb2936d91e3be40e72ef5a922067
Marcelo Serpa <boss@fullofcaffeine.com>
2025-09-15 10:38:07 -0600

    test: update core test expectations to idiomatic Elixir (Phase 0A - 14/65 tests)
    - Updated 14 core tests with idiomatic Elixir patterns
    - Replaced elem() calls with direct pattern matching using atoms
    - Converted reduce_while/Stream.iterate to for comprehensions
    - Fixed string interpolation to use #{} syntax
    - Removed generated variables (g, g1, g2) in favor of meaningful names
    - Cleaned up abstract type naming (removed underscore prefixes)
    - Preserved Log.trace calls as they are intentional compiler output
    
    This establishes baseline idiomatic patterns for the AST modularization refactoring.

--- test/snapshot/core/pattern_matching/intended/pattern_matching_test.ex
+++ test/snapshot/core/pattern_matching/intended/pattern_matching_test.ex
@@ -1,134 +1,103 @@
 defmodule PatternMatchingTest do
-  def match_color(_color) do
-    case (_color) do
-      {:red} ->
+  def match_color(color) do
+    case color do
+      :red ->
         "red"
-      {:green} ->
+      :green ->
         "green"
-      {:blue} ->
+      :blue ->
         "blue"
       {:rgb, r, g, b} ->
-        g = elem(_color, 1)
-        g1 = elem(_color, 2)
-        g2 = elem(_color, 3)
-        r = g
-        g = g1
-        b = g2
-        "rgb(" <> Kernel.to_string(r) <> "," <> Kernel.to_string(g) <> "," <> Kernel.to_string(b) <> ")"
+        "rgb(#{r},#{g},#{b})"
     end
   end
-  def match_option(_option) do
-    case (_option) do
-      {:none} ->
+
+  def match_option(option) do
+    case option do
+      :none ->
         "none"
       {:some, value} ->
-        g = elem(_option, 1)
-        value = g
-        "some(" <> Std.string(value) <> ")"
+        "some(#{inspect(value)})"
     end
   end
+
   def match_int(value) do
-    case (value) do
+    case value do
       0 ->
         "zero"
       1 ->
         "one"
+      n when n < 0 ->
+        "negative"
+      n when n > 100 ->
+        "large"
       _ ->
-        n = value
-        if (n < 0) do
-          "negative"
-        else
-          n = value
-          if (n > 100), do: "large", else: "other"
-        end
+        "other"
     end
   end
+
   def match_string(str) do
-    case (str) do
+    case str do
       "" ->
         "empty"
       "hello" ->
         "greeting"
+      s when byte_size(s) > 10 ->
+        "long"
       _ ->
-        s = str
-        if (length(s) > 10), do: "long", else: "other"
+        "other"
     end
   end
+
   def match_array(arr) do
-    case (length(arr)) do
-      0 ->
+    case arr do
+      [] ->
         "empty"
-      1 ->
-        g = arr[0]
-        x = g
-        "single(" <> Kernel.to_string(x) <> ")"
-      2 ->
-        g = arr[0]
-        g1 = arr[1]
-        x = g
-        y = g1
-        "pair(" <> Kernel.to_string(x) <> "," <> Kernel.to_string(y) <> ")"
-      3 ->
-        g = arr[0]
-        g1 = arr[1]
-        g2 = arr[2]
-        x = g
-        y = g1
-        z = g2
-        "triple(" <> Kernel.to_string(x) <> "," <> Kernel.to_string(y) <> "," <> Kernel.to_string(z) <> ")"
+      [x] ->
+        "single(#{x})"
+      [x, y] ->
+        "pair(#{x},#{y})"
+      [x, y, z] ->
+        "triple(#{x},#{y},#{z})"
       _ ->
         "many"
     end
   end
-  def match_nested(_option) do
-    case (_option) do
-      {:none} ->
+
+  def match_nested(option) do
+    case option do
+      :none ->
         "no color"
-      {:some, value} ->
-        g = elem(_option, 1)
-        case (g) do
-          {:red} ->
-            "red color"
-          {:green} ->
-            "green color"
-          {:blue} ->
-            "blue color"
-          {:rgb, r, g, b} ->
-            g1 = g
-            g2 = g
-            g = g
-            r = g1
-            g = g2
-            b = g
-            if (r > 128) do
-              "bright rgb"
-            else
-              r = g1
-              g = g2
-              b = g
-              "dark rgb"
-            end
-        end
+      {:some, :red} ->
+        "red color"
+      {:some, :green} ->
+        "green color"
+      {:some, :blue} ->
+        "blue color"
+      {:some, {:rgb, r, g, b}} when r > 128 ->
+        "bright rgb"
+      {:some, {:rgb, r, g, b}} ->
+        "dark rgb"
     end
   end
+
   def match_bool(flag, count) do
-    if flag do
-      if (count == 0) do
+    case {flag, count} do
+      {true, 0} ->
         "true zero"
-      else
-        n = count
-        if (n > 0), do: "true positive", else: "other combination"
-      end
-    else
-      if (count == 0) do
+      {false, 0} ->
         "false zero"
-      else
-        n = count
-        if (n > 0), do: "false positive", else: "other combination"
-      end
+      {true, n} when n > 0 ->
+        "true positive"
+      {false, n} when n > 0 ->
+        "false positive"
+      _ ->
+        "other combination"
     end
   end
+
   def main() do
+    # Test entry point for compilation
     Log.trace("Pattern matching compilation test", %{:file_name => "Main.hx", :line_number => 110, :class_name => "PatternMatchingTest", :method_name => "main"})
   end
 end
