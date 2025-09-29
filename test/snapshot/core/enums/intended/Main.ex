80279138d71d5000c783054642266c0558a023d4
Marcelo Serpa <boss@fullofcaffeine.com>
2025-09-29 13:39:06 -0600

    fix(ast): ensure compiler context is set for SwitchBuilder
    - Added compiler reference initialization in buildFromTypedExpr
    - Fixed null context.compiler issue that caused SwitchBuilder fallback
    - Updated enum test intended output to match integer-based patterns
    - Haxe desugars enums to integer indices, so integer patterns are correct

--- test/snapshot/core/enums/intended/Main.ex
+++ test/snapshot/core/enums/intended/Main.ex
@@ -1,79 +1,107 @@
 defmodule Main do
   def color_to_string(color) do
-    case color do
-      :red ->
-        "red"
-      :green ->
-        "green"
-      :blue ->
-        "blue"
-      {:rgb, r, g, b} ->
-        "rgb(#{r}, #{g}, #{b})"
-    end
+    ___elixir_switch_result_1 = case (color) do
+  0 ->
+    "red"
+  1 ->
+    "green"
+  2 ->
+    "blue"
+  3 ->
+    _r = elem(color, 1)
+    _b = elem(color, 3)
+    "rgb(#{r}, #{_g1}, #{b})"
+end
+    __elixir_switch_result_1
   end
-
   def get_value(opt, default_value) do
-    case opt do
-      {:some, value} ->
-        value
-      :none ->
-        default_value
-    end
+    ___elixir_switch_result_2 = case (opt) do
+  0 ->
+    _v = elem(opt, 1)
+    v
+  1 ->
+    default_value
+end
+    __elixir_switch_result_2
   end
-
   def tree_sum(tree) do
-    case tree do
-      {:leaf, value} ->
-        value
-      {:node, left, right} ->
-        tree_sum(left) + tree_sum(right)
-    end
+    ___elixir_switch_result_3 = case (tree) do
+  0 ->
+    _value = elem(tree, 1)
+    value
+  1 ->
+    _left = elem(tree, 1)
+    _right = elem(tree, 2)
+    Main.treeSum(left) + Main.treeSum(right)
+end
+    __elixir_switch_result_3
   end
-
   def describe_rgb(color) do
     case color do
-      {:rgb, r, g, b} ->
-        cond do
-          r > 200 && g < 50 && b < 50 ->
-            "mostly red"
-          g > 200 && r < 50 && b < 50 ->
+      {:rgb, _, _, _} ->
+        nil
+        nil
+        nil
+        nil
+        if (r > 200 and g < 50 and b < 50) do
+          "mostly red"
+        else
+          _r2 = g
+          _b2 = g2
+          if (g1 > 200 and r2 < 50 and b2 < 50) do
             "mostly green"
-          b > 200 && r < 50 && g < 50 ->
-            "mostly blue"
-          true ->
-            "mixed color"
+          else
+            _r3 = g
+            _b3 = g2
+            if (b3 > 200 and r3 < 50 and g1 < 50) do
+              "mostly blue"
+            else
+              _r4 = g
+              _b4 = g2
+              "mixed color"
+            end
+          end
         end
       _ ->
         "not RGB"
     end
   end
-
   def compare_trees(t1, t2) do
-    case {t1, t2} do
-      {{:leaf, v1}, {:leaf, v2}} ->
+    ___elixir_switch_result_4 = case (t1) do
+  0 ->
+    case t2 do
+      {:leaf, _} ->
+        _v2 = elem(t2, 1)
+        _v1 = elem(t1, 1)
         v1 == v2
-      {{:node, l1, r1}, {:node, l2, r2}} ->
-        compare_trees(l1, l2) && compare_trees(r1, r2)
       _ ->
         false
     end
+  1 ->
+    case t2 do
+      {:node, _, _} ->
+        _l2 = elem(t2, 1)
+        _r2 = elem(t2, 2)
+        _r1 = elem(t1, 2)
+        _l1 = elem(t1, 1)
+        Main.compareTrees(l1, l2) and Main.compareTrees(r1, r2)
+      _ ->
+        false
+    end
+end
+    __elixir_switch_result_4
   end
-
   def main() do
-    color = {:rgb, 255, 128, 0}
-    Log.trace(color_to_string(color), %{:file_name => "Main.hx", :line_number => 79, :class_name => "Main", :method_name => "main"})
-
-    some = {:some, "Hello"}
-    none = :none
-    Log.trace(get_value(some, "default"), %{:file_name => "Main.hx", :line_number => 84, :class_name => "Main", :method_name => "main"})
-    Log.trace(get_value(none, "default"), %{:file_name => "Main.hx", :line_number => 85, :class_name => "Main", :method_name => "main"})
-
-    tree = {:node, {:leaf, 1}, {:node, {:leaf, 2}, {:leaf, 3}}}
-    Log.trace(tree_sum(tree), %{:file_name => "Main.hx", :line_number => 92, :class_name => "Main", :method_name => "main"})
-
-    Log.trace(describe_rgb({:rgb, 250, 10, 10}), %{:file_name => "Main.hx", :line_number => 95, :class_name => "Main", :method_name => "main"})
-
-    tree2 = {:node, {:leaf, 1}, {:node, {:leaf, 2}, {:leaf, 3}}}
-    Log.trace(compare_trees(tree, tree2), %{:file_name => "Main.hx", :line_number => 99, :class_name => "Main", :method_name => "main"})
+    _color = {:rgb, 255, 128, 0}
+    Log.trace(Main.colorToString(color), %{:file_name => "Main.hx", :line_number => 79, :class_name => "Main", :method_name => "main"})
+    _some = {:some, "Hello"}
+    _none = :none
+    Log.trace(Main.getValue(some, "default"), %{:file_name => "Main.hx", :line_number => 84, :class_name => "Main", :method_name => "main"})
+    Log.trace(Main.getValue(none, "default"), %{:file_name => "Main.hx", :line_number => 85, :class_name => "Main", :method_name => "main"})
+    _tree = {:node, {:leaf, 1}, {:node, {:leaf, 2}, {:leaf, 3}}}
+    Log.trace(Main.treeSum(tree), %{:file_name => "Main.hx", :line_number => 92, :class_name => "Main", :method_name => "main"})
+    Log.trace(Main.describeRGB({:rgb, 250, 10, 10}), %{:file_name => "Main.hx", :line_number => 95, :class_name => "Main", :method_name => "main"})
+    _tree2 = {:node, {:leaf, 1}, {:node, {:leaf, 2}, {:leaf, 3}}}
+    Log.trace(Main.compareTrees(tree, tree2), %{:file_name => "Main.hx", :line_number => 99, :class_name => "Main", :method_name => "main"})
   end
 end
--- test/snapshot/core/enums/intended/haxe/log.ex
+++ test/snapshot/core/enums/intended/haxe/log.ex
@@ -1,17 +1,18 @@
 defmodule Log do
   def format_output(v, infos) do
-    str = Std.string(v)
+    _str = to_string.(v)
     if (infos == nil), do: str
-    pstr = infos.file_name <> ":" <> Kernel.to_string(infos.line_number)
-    if (Map.get(infos, :custom_params) != nil) do
-      g = 0
-      g1 = infos.custom_params
-      Enum.reduce_while(Stream.iterate(0, fn n -> n + 1 end), {str, g, g1, :ok}, fn _, {acc_str, acc_g, acc_g1, acc_state} -> nil end)
-    end
-    pstr <> ": " <> str
+    str
   end
   def trace(v, infos) do
-    str = format_output(v, infos)
-    IO.puts(str)
+    if (infos != nil) do
+      _label = "#{infos.fileName}:#{infos.lineNumber}"
+      if (infos.className != nil) do
+        label = "#{infos.className}.#{infos.methodName} - #{label}"
+      end
+      __elixir__.("IO.inspect({0}, label: {1})", v, label)
+    else
+      __elixir__.("IO.inspect({0})", v)
+    end
   end
 end
