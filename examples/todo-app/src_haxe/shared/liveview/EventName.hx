129a2ead74359294db88065eb08589c919ebfea3
Marcelo Serpa <boss@fullofcaffeine.com>
2026-01-03 00:25:04 -0600

    todo-app: enforce typed phx-* events via EventName

--- examples/todo-app/src_haxe/shared/liveview/EventName.hx
+++ examples/todo-app/src_haxe/shared/liveview/EventName.hx
@@ -15,7 +15,38 @@ package shared.liveview;
  */
 @:phxEventNames
 enum abstract EventName(String) from String to String {
+    // TodoLive
     var ToggleForm = "toggle_form";
     var CreateTodo = "create_todo";
-}
+    var SaveTodo = "save_todo";
+    var EditTodo = "edit_todo";
+    var DeleteTodo = "delete_todo";
+    var CancelEdit = "cancel_edit";
+    var ToggleTodo = "toggle_todo";
 
+    var FilterTodos = "filter_todos";
+    var SearchTodos = "search_todos";
+    var SortTodos = "sort_todos";
+    var ToggleTag = "toggle_tag";
+    var SetPriority = "set_priority";
+
+    var BulkComplete = "bulk_complete";
+    var BulkDeleteCompleted = "bulk_delete_completed";
+    var BulkSetPriority = "bulk_set_priority";
+
+    // UsersLive
+    var FilterUsers = "filter_users";
+    var ToggleActive = "toggle_active";
+
+    // OrganizationLive
+    var SwitchOrg = "switch_org";
+    var InviteOrg = "invite_org";
+    var RevokeInvite = "revoke_invite";
+    var SetUserRole = "set_user_role";
+
+    // ProfileLive
+    var SaveProfile = "save_profile";
+
+    // AuditLogLive
+    var FilterAudit = "filter_audit";
+}
