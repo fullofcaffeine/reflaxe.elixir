f63ad1df1099f93fbedc90df2ad79af9e93c1c53
Marcelo Serpa <boss@fullofcaffeine.com>
2026-01-03 23:47:54 -0600

    cleanup: remove unused test debug artifacts

--- test/debug_loop/TestLoopDebug.hx
+++
@@ -1,9 +0,0 @@
-class TestLoopDebug {
-    public static function main() {
-        var items = [];
-        for (i in 0...5) {
-            items.push("Item " + i);
-        }
-        trace(items);
-    }
-}
