478639c92a16aa69423472e52cad2d60b5dbb307
Marcelo Serpa <boss@fullofcaffeine.com>
2026-02-09 21:29:09 -0600

    vendor(genes): drop upstream tests
    Remove vendor/genes/tests from this repo; our genes library classpath only uses vendor/genes/src, so keeping upstream tests adds noise without affecting builds.

--- vendor/genes/tests/enum.js
+++
@@ -1,3 +0,0 @@
-export const A = 1;
-export const B = 2;
-export default null;
--- vendor/genes/tests/foo/MyClass.hx
+++
@@ -1,8 +0,0 @@
-package tests.foo;
-
-class MyClass {
-  public function new() {}
-
-  public function toString()
-    return 'foo';
-}
