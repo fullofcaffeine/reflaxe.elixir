a23db1389493f3d05c343d193d5b157c0c5e1832
Marcelo Serpa <boss@fullofcaffeine.com>
2026-02-12 08:47:51 -0600

    Track phoenix chat web scaffold modules for CI smoke

---
+++ examples/12-phoenix-chat/lib/phoenix_chat_web/router.ex
@@ -0,0 +1,28 @@
+defmodule PhoenixChatWeb.Router do
+  use PhoenixChatWeb, :router
+  import Phoenix.LiveView.Router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {PhoenixChatWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", PhoenixChatWeb do
+    pipe_through :browser
+
+    live "/", AppLive, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", PhoenixChatWeb do
+  #   pipe_through :api
+  # end
+end
